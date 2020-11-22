790b998e5730931a15ac8cb13f0ebd2ef2aad86d
finster77 <timmytom444@gmail.com>
2020-11-22 23:00:15 +0000

    what is going on here

---
+++ Sub-ordinatesPrototype/Assets/Scripts/ControllerInputDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputDetection : MonoBehaviour
+{
+
+    private Vector3 _movement = Vector3.zero;
+    private string _xAxis, _yAxis;
+    public float speed;
+
+    void Start()
+    {
+        _xAxis = "Horizontal";
+        _yAxis = "Vertical";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement = new Vector3(Input.GetAxisRaw(_xAxis), 0, Input.GetAxisRaw(_yAxis));
+        Debug.Log(_movement);
+       
+    }
+
+}
