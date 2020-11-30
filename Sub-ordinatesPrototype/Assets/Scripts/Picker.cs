797079219c57f96c9a463382625baacd4123d0c2
finster77 <timmytom444@gmail.com>
2020-11-30 11:31:26 +0000

    menu Sounds

---
+++ Sub-ordinatesPrototype/Assets/Scripts/Picker.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Picker : MonoBehaviour
+{
+    public Transform celt, greek, japanese;
+    public int player;
+    int i;
+    bool A;
+    bool selecting;
+    static bool cSelected, gSelected, jSelected;
+   public TeamManager team;
+    public float VerticalInput;
+    public bool picked;
+
+    public Image[] images;
+   
+    public Sprite[] Celts,greeks,japan;
+
+    public AudioSource move;
+    public AudioSource selected;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        VerticalInput = Input.GetAxis("Vertical" + player);
+        A = Input.GetButton("Jump" + player);
+        if ( selecting==false )
+            {
+
+                StartCoroutine(Select());
+                if (i == 0)
+                {
+                    transform.position = japanese.position;
+                    i = 3;
+                }
+                if (i == 1)
+                {
+                    transform.position = celt.position;
+                }
+                if (i == 2)
+                {
+
+                    transform.position = greek.position;
+                }
+                if (i == 3)
+                {
+                    transform.position = japanese.position;
+                }
+                if (i == 4)
+                {
+
+                    transform.position = celt.position;
+                    i = 1;
+                }
+
+            if (A)
+            {
+                if (i == 1 && team.team[0]==0)
+                {
+                    //celt
+                  
+                    team.team[0]=player;
+                    images[0].sprite = Celts[0];
+                    images[1].sprite = Celts[1];
+                    images[2].sprite = Celts[2];
+                    picked = true;
+                    selected.Play();
+
+                }
+                if (i == 2 && team.team[1] == 0)
+                {
+                    //greek
+                    
+                    team.team[1] = player;
+                    images[0].sprite = greeks[0];
+                    images[1].sprite = greeks[1];
+                    images[2].sprite = greeks[2];
+                    picked = true;
+                    selected.Play();
+
+                }
+                if (i == 3 && team.team[2] == 0)
+                {
+                    //japan
+                   
+                    team.team[2] = player;
+                    images[0].sprite = japan[0];
+                    images[1].sprite = japan[1];
+                    images[2].sprite = japan[2];
+                    picked = true;
+                    selected.Play();
+                }
+                team.Fucksion(player, i-1);
+            }
+         
+        }
+
+
+       
+
+        
+
+        
+    }
+
+    private IEnumerator Select()
+    {
+       
+            if (VerticalInput < 0)
+            {
+                i++;
+            move.Play();
+        }
+            if (VerticalInput > 0)
+            {
+                i--;
+            move.Play();
+        }
+            selecting = true;
+
+        
+
+        yield return new WaitForSeconds(0.2f);
+        selecting = false;
+       
+
+    }
+}
---
+++ Sub-ordinatesPrototype/Assets/Scripts/Pointer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Pointer : MonoBehaviour
+{
+    public int player;
+    float VerticalInput;
+    float HorizontalInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal" + player);
+        VerticalInput = Input.GetAxis("Vertical" + player);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(HorizontalInput, VerticalInput, 0f);
+        transform.position = transform.position + movement/8;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("im here");
+        if (collision.gameObject.CompareTag ("Start"))
+        {
+            if (Input.GetButtonDown("Jump1"))
+            {
+                Debug.Log("bbbbbb");
+            }
+        }
+    }
+}
