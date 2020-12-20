eb958faf13ff21727b94ca741589787e486b6d73
conor6280 <61231065+conor6280@users.noreply.github.com>
2020-12-20 14:03:13 +0000

    mfsrgn

--- Sub-ordinatesPrototype/Assets/Scripts/ChararacterControllerScript.cs
+++ Sub-ordinatesPrototype/Assets/Scripts/ChararacterControllerScript.cs
@@ -29,6 +29,13 @@ public class ChararacterControllerScript : MonoBehaviour
     bool P;
     bool N;
 
+    public AudioSource JumpSound;
+    public AudioSource PunchSound;
+    public AudioSource LightningSound;
+    public AudioSource BlockSound;
+    public AudioSource SwapSound;
+    public AudioSource DashSound;
+
     public SpriteRenderer spriteRend;
     public TeamController teamController;
 
@@ -90,18 +97,18 @@ public class ChararacterControllerScript : MonoBehaviour
 
     public void CoolReset(bool death)
     {
-         meleeTimer = 0;
-         blockTimer = 0;
-         //ultTimer;
-         meleeCool = 1/ character.attackRate;
-         blockCool = character.blockWait;
-         //ultCool;
-         meleeWait = false ;
-         blockWait=false;
+        meleeTimer = 0;
+        blockTimer = 0;
+        //ultTimer;
+        meleeCool = 1 / character.attackRate;
+        blockCool = character.blockWait;
+        //ultCool;
+        meleeWait = false;
+        blockWait = false;
 
-         //ultWait;
+        //ultWait;
 
-         switchWait = !death;
+        switchWait = !death;
 
         if (death)
         {
@@ -139,7 +146,7 @@ public class ChararacterControllerScript : MonoBehaviour
         //    meleeCooling = true;
         //    //StartCoroutine(MeleeCool(1/character.attackRate));
         //}
-        if(Input.GetButtonUp("Block" + player)&& !blockWait)
+        if (Input.GetButtonUp("Block" + player) && !blockWait)
         {
             blockWait = true;
             blockTimer = blockCool;
@@ -181,6 +188,7 @@ public class ChararacterControllerScript : MonoBehaviour
             switchWait = false;
         }
         UIUpdate();
+
         Switch();
         Block();
         Move();
@@ -215,7 +223,7 @@ public class ChararacterControllerScript : MonoBehaviour
     void UIUpdate()
     {
         cooldowns[0].fillAmount = (meleeCool - meleeTimer) / meleeCool;
-        cooldowns[1].fillAmount = (blockCool-blockTimer)/blockCool;
+        cooldowns[1].fillAmount = (blockCool - blockTimer) / blockCool;
         cooldowns[2].fillAmount = (switchCool - switchTimer) / switchCool;
         //cooldowns[1].fillAmount = (blockCool - blockTimer) / blockCool;
         attackCount.text = meleeStore.ToString();
@@ -227,14 +235,14 @@ public class ChararacterControllerScript : MonoBehaviour
 
 
         float moveBy = HorizontalInput * speed;
-        if (blocking) moveBy *= (1-character.block) ;
+        if (blocking) moveBy *= (1 - character.block);
         rb.velocity = new Vector2(moveBy, rb.velocity.y);
 
-        if(rb.velocity.x > 0)
+        if (rb.velocity.x > 0)
         {
             Turn(1);
         }
-        else if(rb.velocity.x < 0)
+        else if (rb.velocity.x < 0)
         {
             Turn(-1);
         }
@@ -242,13 +250,14 @@ public class ChararacterControllerScript : MonoBehaviour
 
     void Jump()
     {
-        
-        if (A && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor) && rb.velocity.y==0 && !blocking)
+
+        if (A && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor) && rb.velocity.y == 0 && !blocking)
         {
             //spriteRend.color = Color.red;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            JumpSound.Play();
         }
-        
     }
 
     void CheckIfGrounded()
@@ -292,38 +301,43 @@ public class ChararacterControllerScript : MonoBehaviour
             if (character.type == 0)
             {
                 if (HorizontalInput == 0 && VerticalInput == 0) HorizontalInput = transform.localScale.x;
-                Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + new Vector3(transform.localScale.x*0.55f,0), 1.1f, LayerMask.GetMask("Player"));
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + new Vector3(transform.localScale.x * 0.55f, 0), 1.1f, LayerMask.GetMask("Player"));
                 //RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, new Vector2(HorizontalInput, VerticalInput), 1.5f, LayerMask.GetMask("Player"));
+
+                PunchSound.Play();
+
                 if (colliders.Length > 1)
                 {
                     enemy.Hurt(character.attack);
                 }
             }
-            else if(character.type == 1)
+            else if (character.type == 1)
             {
                 if (HorizontalInput == 0 && VerticalInput == 0) HorizontalInput = transform.localScale.x;
-                GameObject proj = Instantiate(rangeAttack, transform.position, Quaternion.Euler(0,0,-90+Mathf.Rad2Deg*Mathf.Atan2(VerticalInput, HorizontalInput)));
+                GameObject proj = Instantiate(rangeAttack, transform.position, Quaternion.Euler(0, 0, -90 + Mathf.Rad2Deg * Mathf.Atan2(VerticalInput, HorizontalInput)));
                 proj.name = character.attack.ToString();
                 proj.tag = projTag;
                 Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
                 rb.AddForce(proj.transform.up * 9, ForceMode2D.Impulse);
                 Destroy(proj, 5f);
 
+                LightningSound.Play();
+
             }
-            else if(character.type == 2)
+            else if (character.type == 2)
             {
                 dashOverride = true;
                 Vector2 start = transform.position;
                 StartCoroutine(DashWait(start));
 
                 if (HorizontalInput == 0 && VerticalInput == 0) HorizontalInput = transform.localScale.x;
-                rb.velocity = new Vector2(HorizontalInput, VerticalInput*0.7f) * 30;
-                
+                rb.velocity = new Vector2(HorizontalInput, VerticalInput * 0.7f) * 30;
 
+                DashSound.Play();
             }
             meleeStore--;
         }
-        
+
     }
 
     IEnumerator DashWait(Vector2 start)
@@ -334,11 +348,11 @@ public class ChararacterControllerScript : MonoBehaviour
 
         float xChange = Mathf.Abs(transform.position.x - start.x);
         float yChange = Mathf.Abs(transform.position.y - start.y);
-        float angle = Mathf.Rad2Deg*Mathf.Atan2(yChange,xChange);
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(yChange, xChange);
         float dist = Vector2.Distance(transform.position, start);
         Vector2 dir = new Vector2(0, 0);
 
-        RaycastHit2D[] colliders = Physics2D.BoxCastAll(start, new Vector2(1,1), angle, new Vector2(xChange,yChange),dist, LayerMask.GetMask("Player"));
+        RaycastHit2D[] colliders = Physics2D.BoxCastAll(start, new Vector2(1, 1), angle, new Vector2(xChange, yChange), dist, LayerMask.GetMask("Player"));
 
         if (colliders.Length > 1)
         {
@@ -350,13 +364,15 @@ public class ChararacterControllerScript : MonoBehaviour
 
     void Switch()
     {
-        if ((P || N ) && teamController.deadCount < 2 && !switchWait)//&& !switchCooling)
+        if ((P || N) && teamController.deadCount < 2 && !switchWait)//&& !switchCooling)
         {
             int dir = 0;
             if (P) dir = -1;
             else if (N) dir = 1;
 
             teamController.NewChar(dir, false);
+
+            SwapSound.Play();
         }
 
     }
@@ -391,20 +407,21 @@ public class ChararacterControllerScript : MonoBehaviour
 
     public void Hurt(int dmg)
     {
-        
+
         if (blocking)
         {
             float newDmg = dmg * (1 - character.block);
             dmg = Mathf.RoundToInt(newDmg);
+            BlockSound.Play();
         }
         teamController.Hurt(dmg);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Proj1" || collision.tag == "Proj2")
+        if (collision.tag == "Proj1" || collision.tag == "Proj2")
         {
-            if(collision.tag != projTag)
+            if (collision.tag != projTag)
             {
                 if (dashOverride) return;
                 Hurt(int.Parse(collision.name));
